c9416b87cfa6e0af4c94d46fadf364702ecef712
[ayush tiwari] <[ayush0511tiwari@gmail.com]>
2024-07-04 21:27:06 +0530

    upload

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,20 @@
+
+
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+        <main>
+            LEFT SIDEBAR  
+            {children} 
+            RIGHT SIDEBAR  
+        </main>
+    </div>
+    
+  );
+}
